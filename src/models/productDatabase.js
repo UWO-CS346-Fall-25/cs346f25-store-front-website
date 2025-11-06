9eec28b0f976cf23e4a0cf38376f4d1c30cfdb51
CloseRange <michael.m.hulbert@gmail.com>
2025-11-06 13:51:54 -0600

    Remove mock data and refactor product database logic
    Deleted mock data and mock database modules, and refactored productDatabase.js to remove mock dependencies and improve cache key usage. Added new helper functions for binding images and categories to products, and updated category and product retrieval methods. Updated admin products view to remove the price column and display price within the product name cell.

--- src/models/productDatabase.js
+++ src/models/productDatabase.js
@@ -5,71 +5,88 @@ const debug = require('../controllers/debug');
 const database = require('./db.js');
 const mock = require('./mock/productDatabase.js');
 
-
-async function bindImagesToProducts(res) {
-  for (let r of res.rows) {
-    const prodImg = await database.query('SELECT path, external_url, alt FROM public.product_images WHERE product_id = $1 AND is_primary = true LIMIT 1', [r.id]);
-    prodImg.rows[0].img_url = prodImg.rows[0].path == null ? prodImg.rows[0].external_url : prodImg.rows[0].path;
-    r.image = prodImg.rows[0];
-  }
-  return res;
-}
+const NAMESPACE = 'productDB';
+const TTL = 60_000; // TODO: adjust cache TTL as needed (should be higher / can clear cache as needed)
 
 
-module.exports = {
-  ttl: 60_000,
-  namespace: 'productDB',
-  getAllProducts: async function () {
-    const key = `${this.namespace}:allProducts`;
-    return cache.wrap(key, this.ttl, async () => {
-      const res = await database.query('SELECT * FROM public.products');
-      return res.rows;
-    });
-  },
-  getAllProductsWithMainImage: async function () {
-    const key = `${this.namespace}:allProductsWithMainImage`;
-    return cache.wrap(key, this.ttl, async () => {
-      const res = await database.query('SELECT * FROM public.products');
-      await bindImagesToProducts(res);
-      return res.rows;
-    });
-  },
-  getProduct: async function (id) {
-    const key = `${this.namespace}:product:${id}`;
-    return cache.wrap(key, this.ttl, async () => {
-      const res = await database.query('SELECT * FROM public.products WHERE id = $1', [id]);
-      return res.rows[0] || null;
-    });
-  },
-  getProductBySlug: async function (slug) {
-    const key = `${this.namespace}:productBySlug:${slug}`;
-    return cache.wrap(key, this.ttl, async () => {
-      const res = await database.query('SELECT * FROM public.products WHERE slug = $1', [slug]);
-      return res.rows[0] || null;
-    });
-  },
-  getProductWithImage: async function (id) {
-    if (debug.isMockDB()) return mock.getProductWithImage();
-    const key = `${this.namespace}:product:${id}`;
-    return cache.wrap(key, this.ttl, async () => {
-      const res = await database.query('SELECT * FROM public.products WHERE id = $1', [id]);
-      await bindImagesToProducts(res);
-      return res.rows[0];
-    });
-  },
-  getImagesForProduct: async function (productId) {
-    const key = `${this.namespace}:productImages:${productId}`;
-    return cache.wrap(key, this.ttl, async () => {
-      const res = await database.query('SELECT path, external_url, alt FROM public.product_images WHERE product_id = $1 ORDER BY is_primary DESC, id ASC', [productId]);
+async function bindImages(products) {
+  for (let p of products) {
+    const key = `${NAMESPACE}:product:${p.id}:images`;
+    p.images = await cache.wrap(key, TTL, async () => {
+      const res = await database.query('SELECT path, external_url, alt FROM public.product_images WHERE product_id = $1 ORDER BY is_primary DESC, id ASC', [p.id]);
       for (let r of res.rows) {
         r.img_url = r.path == null ? r.external_url : r.path;
       }
       return res.rows;
     });
+  }
+}
+// takes an array of products and binds 1 primary images to them
+async function bindPrimaryImage(products) {
+  for (let p of products) {
+    const key = `${NAMESPACE}:product:${p.id}:primaryImage`;
+    p.image = await cache.wrap(key, TTL, async () => {
+      const prodImg = await database.query('SELECT path, external_url, alt FROM public.product_images WHERE product_id = $1 AND is_primary = true LIMIT 1', [p.id]);
+      if (prodImg.rows.length === 0) return null;
+      return {
+        img_url: prodImg.rows[0].path == null ? prodImg.rows[0].external_url : prodImg.rows[0].path,
+        alt: prodImg.rows[0].alt,
+      };
+    });
+  }
+}
+
+async function bindCategories(products) {
+  for (let p of products) {
+    const key = `${NAMESPACE}:product:${p.id}:categories`;
+    p.categories = await cache.wrap(key, TTL, async () => {
+      const res = await database.query('SELECT c.id, c.name, c.slug FROM public.categories c JOIN public.product_categories pc ON c.id = pc.category_id WHERE pc.product_id = $1', [p.id]);
+      return res.rows;
+    });
+  }
+}
+
+async function categoryBindProducts(categories) {
+  for (let c of categories) {
+    const key = `${NAMESPACE}:category:${c.id}:products`;
+    c.products = await cache.wrap(key, TTL, async () => {
+      const res = await database.query('SELECT p.* FROM public.products p JOIN public.product_categories pc ON p.id = pc.product_id WHERE pc.category_id = $1', [c.id]);
+      return res.rows;
+    });
+  }
+}
+
+
+
+module.exports = {
+  bindImages,
+  bindPrimaryImage,
+  bindCategories,
+  categoryBindProducts,
+
+  getAll: async function () {
+    const key = `${NAMESPACE}:allProducts`;
+    return cache.wrap(key, TTL, async () => {
+      const res = await database.query('SELECT * FROM public.products');
+      return res.rows;
+    });
+  },
+  getByID: async function (id) {
+    const key = `${NAMESPACE}:product:${id}`;
+    return cache.wrap(key, this.ttl, async () => {
+      const res = await database.query('SELECT * FROM public.products WHERE id = $1', [id]);
+      return res.rows[0] || null;
+    });
+  },
+  getBySlug: async function (slug) {
+    const key = `${NAMESPACE}:product:${slug}:slug`;
+    return cache.wrap(key, this.ttl, async () => {
+      const res = await database.query('SELECT * FROM public.products WHERE slug = $1', [slug]);
+      return res.rows[0] || null;
+    });
   },
   getFeatured: async function () {
-    if (debug.isMockDB()) return mock.getFeatured();
-    const key = `${this.namespace}:featured`;
+    const key = `${NAMESPACE}:featured`;
     return cache.wrap(key, this.ttl, async () => {
       const res = await database.query('SELECT * FROM public.v_featured_products order by position');
       for (let r of res.rows) {
@@ -79,29 +96,61 @@ module.exports = {
     });
   },
   getCategories: async function () {
-    if (debug.isMockDB()) return mock.getCategories();
-    const key = `${this.namespace}:categories`;
+    const key = `${NAMESPACE}:categories`;
+    return cache.wrap(key, this.ttl, async () => {
+      const res = await database.query('SELECT * FROM categories order by position');
+      return res.rows;
+    });
+  },
+  getActiveCategories: async function () {
+    const key = `${NAMESPACE}:activeCategories`;
     return cache.wrap(key, this.ttl, async () => {
       const res = await database.query('SELECT * FROM categories where is_active = true order by position');
-      for (let r of res.rows) {
-        const prods = await database.query('SELECT product_id FROM product_categories WHERE category_id = $1', [r.id]);
-        let prods_delta = [];
-        for (let p of prods.rows) {
-          prods_delta.push(await this.getProductWithImage(p.product_id));
-        }
-        r.products = prods_delta;
-      }
       return res.rows;
+    });
+  },
+  getCategory: async function (idOrSlug) {
+    const key = `${NAMESPACE}:category:${idOrSlug}`;
+    return cache.wrap(key, this.ttl, async () => {
+      let res;
+      if (isNaN(Number(idOrSlug))) {
+        res = await database.query('SELECT * FROM categories WHERE slug = $1', [idOrSlug]);
+      } else {
+        res = await database.query('SELECT * FROM categories WHERE id = $1', [idOrSlug]);
+      }
+      return res.rows[0] || null;
     });
   },
   getNewArrivals: async function (count) {
-    if (debug.isMockDB()) return mock.getNewArrivals(count);
-    const key = `${this.namespace}:newarrivals:${count}`;
+    const key = `${NAMESPACE}:newarrivals:${count}`;
 
     return cache.wrap(key, this.ttl, async () => {
       const res = await database.query('SELECT * FROM products ORDER BY created_at DESC LIMIT $1', [count]);
-      await bindImagesToProducts(res);
       return res.rows;
     });
   },
+  uncacheProduct: async function (id, slug = null) {
+    if (!slug) {
+      const res = await database.query('SELECT slug FROM public.products WHERE id = $1', [id]);
+      slug = res.rows[0]?.slug;
+    }
+    const keys = [
+      `${NAMESPACE}:product:${id}`,
+      slug ? `${NAMESPACE}:product:${slug}` : null,
+      `${NAMESPACE}:newarrivals`,
+      `${NAMESPACE}:allProducts`,
+    ].filter(Boolean);
+    keys.forEach(k => cache.clearNamespace(k));
+  },
+  uncacheCategory: async function (id, slug = null) {
+    if (!slug) {
+      const res = await database.query('SELECT slug FROM public.categories WHERE id = $1', [id]);
+      slug = res.rows[0]?.slug;
+    }
+    const keys = [
+      `${NAMESPACE}:category:${id}`,
+      slug ? `${NAMESPACE}:category:${slug}` : null,
+    ].filter(Boolean);
+    keys.forEach(k => cache.clearNamespace(k));
+  }
 };
